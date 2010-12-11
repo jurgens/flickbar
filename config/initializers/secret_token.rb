64d0242f2455993c13d34d42a3c12c29ea4d2a6b
Yuri Omelchuk <jurgen@upscript.com>
2010-12-11 21:21:03 +0200

    empty rails app with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flickbar2::Application.config.secret_token = '4ff86aab5cc0d1b0d36b76ee671b522a408154acb21ddde4532456a744b5f23f742a81cdaf1d931ab14bf44e7a7396629c35ec50ec445a7f1e0921b08af4f2ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Flickbar2::Application.config.session_store :cookie_store, :key => '_flickbar2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Flickbar2::Application.config.session_store :active_record_store
