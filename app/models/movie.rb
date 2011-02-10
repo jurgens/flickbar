9cfe8f78321613594d9f5e41730a49d91214b518
Yuri Omelchuk <jurgen@upscript.com>
2011-02-10 13:09:53 +0200

    validate presence of movie title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ActiveRecord::Base
   has_many :watches
   has_many :users, :through => :watches
 
+  validates :title, :presence => true
+
   scope :recently_watched, lambda { joins(:watches).select("DISTINCT movies.*").order("watches.created_at DESC").limit(25)}
 
   def self.find_or_create(title)
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Movie do
+
+  it { should validate_presence_of :title }
+
   it "should find or create movie by title" do
     title = 'The Godfather'
     Movie.find_or_create(title).should_not be_nil
