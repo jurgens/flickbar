787393731d7d05b9f981e6fa0ee7290d8a2a8610
Yuri Omelchuk <jurgen@upscript.com>
2011-03-19 01:36:07 +0200

    news should only be allowed to signed in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-#  before_filter :authenticate_user!
+  before_filter :authenticate_user!, :only => [:news]
   before_filter :find_owner, :only => [:show]
 
   def show
